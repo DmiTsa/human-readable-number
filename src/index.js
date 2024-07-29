50d2a6deefb4eab73cf98ea1359b395184064ae5
DmiTsa <dmitrytaskaev.dt@gmail.com>
2024-07-29 23:30:22 +0300

    feat: 90 per

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+
+    const strNum = {
+        toTen: ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'],
+        twenty: ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'],
+        toHundred: ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    }
+
+    if (!number) { return `zero` }
+
+    const dgt = number.toString().split('').map(Number).reverse()
+
+    let numsToTen = strNum.toTen[dgt[0] - 1] ? `${strNum.toTen[dgt[0] - 1]}` : ``
+
+    dgt[1] === 1 ? numsToTen = strNum.twenty[dgt[0] - 1] : numsToTen
+
+    const numsToHundred = strNum.toHundred[dgt[1] - 2] ? `${strNum.toHundred[dgt[1] - 2]} ` : ``
+
+    const hundreds = dgt.length > 2 ? `${strNum.toTen[dgt[2] - 1]} hundred${numsToHundred || numsToTen ? ` ` : ``}` : ``
+
+    return `${hundreds}${numsToHundred}${numsToTen}`
 }
+
+
